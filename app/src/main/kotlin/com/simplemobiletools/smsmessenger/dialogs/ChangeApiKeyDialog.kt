8419e3e8edbb55661e1ec2d9ee5fc98ef5f2df5f
Denis <denis93rd@gmail.com>
2023-09-04 10:12:25 +0400

    working on link

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/dialogs/ChangeApiKeyDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/dialogs/ChangeApiKeyDialog.kt
@@ -1,6 +1,7 @@
 package com.simplemobiletools.smsmessenger.dialogs
 
 import GoogleTranslate
+import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AlertDialog
 import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.smsmessenger.R
@@ -10,14 +11,14 @@ import com.simplemobiletools.smsmessenger.extensions.config
 
 class ChangeApiKeyDialog(
     private val activity: SimpleActivity,
-    private val googleTranslate: GoogleTranslate, // Add this
-//    private val callback: (apiKey: String) -> Unit,
+    private val googleTranslate: GoogleTranslate,
 ) {
     private val config = activity.config
 
     init {
         val binding = DialogGoogleApiKeyBinding.inflate(activity.layoutInflater).apply {
-            dialogGoogleApiKey.setText(googleTranslate.retrieveApiKey()) // Load the API key when initialized
+            dialogGoogleApiKey.setText(googleTranslate.retrieveApiKey())
+//            tutorialLink.movementMethod = LinkMovementMethod.getInstance()
         }
 
         activity.getAlertDialogBuilder()
@@ -31,7 +32,6 @@ class ChangeApiKeyDialog(
                             apiKey.isEmpty() -> activity.toast(com.simplemobiletools.commons.R.string.empty_name)
                             else -> {
                                 googleTranslate.saveApiKey(apiKey) // Save the API key when confirmed
-//                                callback(apiKey)
                                 alertDialog.dismiss()
                             }
                         }
